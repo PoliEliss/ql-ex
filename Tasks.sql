4d3b79a672610cb4ec536c3e0252b014954ebda6
PoliEliss <96927298+PoliEliss@users.noreply.github.com>
2023-04-18 17:03:56 +0300

    Task5 added

--- Tasks.sql
+++ Tasks.sql
@@ -28,6 +28,12 @@ WHERE Laptop.price > 1000
 SELECT * FROM Printer
 WHERE Printer.color = 'y'
 
+Задание5
+
+Найдите номер модели, скорость и размер жесткого диска ПК, имеющих 12x или 24x CD и цену менее 600 дол
+
+SELECT model, speed, hd FROM PC
+WHERE cd IN ('12x','24x')  AND price < 600
 
 
 Задание 6
